a7a190ee4259ebbd051cff24583ec58e73334eeb
ikhlas <ikhlas_t@apps.ipb.ac.id>
2020-12-11 18:04:40 +0700

    layout titip new

---
+++ app/src/main/java/com/ikhlast/warehouseipb/Fragment/TitipBarangFragment.java
@@ -0,0 +1,64 @@
+package com.ikhlast.warehouseipb.Fragment;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.ikhlast.warehouseipb.Main.Biodata;
+import com.ikhlast.warehouseipb.Main.Titip;
+import com.ikhlast.warehouseipb.Preferences.Sessions;
+import com.ikhlast.warehouseipb.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TitipBarangFragment extends Fragment {
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private DatabaseReference database;
+    AlertDialog.Builder alert;
+    Sessions sessions;
+    String nick;
+    private Button titip;
+    private ProgressDialog loading;
+
+    public TitipBarangFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.titip_barang, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        alert = new AlertDialog.Builder(getContext());
+        database = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        sessions = new Sessions(getContext());
+        user = mAuth.getCurrentUser();
+        nick = user.getEmail().replace("@whipb.com", "");
+    }
+}
--- app/src/main/java/com/ikhlast/warehouseipb/Main/Titip.java
+++ app/src/main/java/com/ikhlast/warehouseipb/Main/Titip.java
@@ -2,12 +2,23 @@ package com.ikhlast.warehouseipb.Main;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -15,13 +26,23 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.ikhlast.warehouseipb.Fragment.TitipBarangFragment;
+import com.ikhlast.warehouseipb.Fragment.TitipHewanFragment;
+import com.ikhlast.warehouseipb.Fragment.TitipKeduanyaFragment;
 import com.ikhlast.warehouseipb.R;
 
-public class Titip extends AppCompatActivity {
+//TODO UPDATE LAYOUT, FAB TITIP BARANG DAN KEDUANYA
+public class Titip extends AppCompatActivity implements View.OnClickListener {
     DatabaseReference db;
     StorageReference dbStorage;
     TextView tvKapasitas;
     int capacity;
+
+    Boolean FH, FB, FHB;
+    FloatingActionButton fab;
+    Button hewan, barang, keduanya;
+    AlertDialog.Builder alert;
+    FragmentManager fm = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +53,16 @@ public class Titip extends AppCompatActivity {
         dbStorage = FirebaseStorage.getInstance().getReference();
         tvKapasitas = findViewById(R.id.titip_kapasitas);
         checkStorage();
+
+        fab = findViewById(R.id.titip_fab);
+        hewan = findViewById(R.id.titip_bt_hewan);
+        barang = findViewById(R.id.titip_bt_barang);
+        keduanya = findViewById(R.id.titip_bt_keduanya);
+
+        fab.setOnClickListener(this);
+        hewan.setOnClickListener(this);
+        barang.setOnClickListener(this);
+        keduanya.setOnClickListener(this);
     }
     private void checkStorage(){
         db.child("Preferences").addValueEventListener(new ValueEventListener() {
@@ -57,5 +88,74 @@ public class Titip extends AppCompatActivity {
 //        super.onBackPressed();
         finish();
         overridePendingTransition(0,0);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.titip_bt_barang:
+                FB = true;
+                FH = false;
+                FHB = false;
+                hewan.setBackgroundResource(R.drawable.tombol2);
+                keduanya.setBackgroundResource(R.drawable.tombol2);
+                barang.setBackgroundResource(R.drawable.tombol);
+                barang.setTextColor(Color.parseColor("#e8d9d6"));
+                hewan.setTextColor(Color.parseColor("#6c411a"));
+                keduanya.setTextColor(Color.parseColor("#6c411a"));
+                loadFrag(new TitipBarangFragment());
+                break;
+            case R.id.titip_bt_hewan:
+                FH = true;
+                FB = false;
+                FHB = false;
+                barang.setBackgroundResource(R.drawable.tombol2);
+                keduanya.setBackgroundResource(R.drawable.tombol2);
+                hewan.setBackgroundResource(R.drawable.tombol);
+                hewan.setTextColor(Color.parseColor("#e8d9d6"));
+                keduanya.setTextColor(Color.parseColor("#6c411a"));
+                barang.setTextColor(Color.parseColor("#6c411a"));
+                loadFrag(new TitipHewanFragment());
+                break;
+            case R.id.titip_bt_keduanya:
+                FHB = true;
+                FH = false;
+                FB = false;
+                hewan.setBackgroundResource(R.drawable.tombol2);
+                barang.setBackgroundResource(R.drawable.tombol2);
+                keduanya.setBackgroundResource(R.drawable.tombol);
+                keduanya.setTextColor(Color.parseColor("#e8d9d6"));
+                hewan.setTextColor(Color.parseColor("#6c411a"));
+                barang.setTextColor(Color.parseColor("#6c411a"));
+                loadFrag(new TitipKeduanyaFragment());
+                break;
+            case R.id.titip_fab:
+                if (FH){
+                    fabOnClick(R.layout.helph);
+                } else if (FB){
+                    fabOnClick(R.layout.helpb);
+                } else {
+                    fabOnClick(R.layout.helphb);
+                }
+                break;
+        }
+    }
+    private boolean loadFrag(Fragment f){
+        if (f != null){
+            fm.beginTransaction().replace(R.id.titip_container, f).commit();
+            return true;
+        }
+        return false;
+    }
+    private void fabOnClick(int l){
+        alert = new AlertDialog.Builder(this);
+        fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.baru_coklatgelapbanget)));
+        alert.setView(l).setTitle("Keterangan").setCancelable(true).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+                fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorAccent)));
+            }
+        }).create().show();
     }
 }
